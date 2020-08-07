a76ee1e714955bb666f6c290c76a617ce641fef6
Ronald Tagolino <tags0516@gmail.com>
2020-08-07 22:30:02 +0800

    Initial views and serializers for appointments module.

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from appointment.models import Appointment, Doctor, Patient
+from appointment.serializers import AppointmentSerializer
+
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    model = Appointment
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
